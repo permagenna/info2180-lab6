1141c6f1ac97c0a48661b9b903caee87b2c342e0
Perma_4thgenna <glendon4b1@gmail.com>
2018-11-13 22:39:41 -0500

    Exercise 4

--- maze.js
+++ maze.js
@@ -7,7 +7,7 @@ window.onload = function(){
 function setUpEvents (){
 	let search = document.getElementById("search_button");
 	let result = document.getElementById("result");
-	
+	let get_all = document.getElementById("get_all");
 	search.onclick = function(){
 		let httpRequest = new XMLHttpRequest();
 		let value = document.getElementById("word").value;
@@ -27,5 +27,53 @@ function setUpEvents (){
 	   	httpRequest.open('GET', url+value, true);
 	   	httpRequest.send();
 	};
+
+	get_all.onclick = function (){
+		let xmlRequest = new XMLHttpRequest();
+		xmlRequest.onreadystatechange = function (){
+			if (xmlRequest.readyState === XMLHttpRequest.DONE) {
+				if (xmlRequest.status === 200) {
+					let response = "<ol>";
+					let text = xmlRequest.responseXML;
+					let terms = text.getElementsByTagName ("definition");
+					for (let i = 0; i < terms.length; i++){
+						response+="<li><h3>"+terms[i].getAttribute("name")+"</h3><p>"+terms[i].childNodes[0].nodeValue+"</p><p> -"+terms[i].getAttribute("author")+"</p></li>";
+					}
+					response+="</ol";
+					let result = document.getElementById("result");
+					result.innerHTML = response;
+				} else {
+					result.innerHTML = ("There was a problem with the request.");
+				} 
+			}
+		};
+		xmlRequest.open("GET","request.php?q=&all=true",true);
+        xmlRequest.send();
+
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- request.php
+++ request.php
@@ -2,14 +2,45 @@
 // accept a term (keyword)
 // respond with a value
 $query = $_GET['q'];
-$definition = [
-    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "A place that sells alcholic beverages",
-    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
-    "html" => "The standard markup language for creating web pages and web applications.",
-    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
-    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
-    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
-];
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+$all= $_GET['all'];
+      
+ $xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+ <entries>
+         <definition name="definition" author="John">
+             a statement of the exact meaning of a word, especially in
+             a dictionary.
+         </definition>
+         <definition name="bar" author="mary">
+             a place that sells alcholic beverages
+         </definition>
+         <definition name="ajax" author="Kimberly">
+             technique which involves the use of javascript and xml
+         </definition>
+         <definition name="html" author="Bob">
+             The standard markup language for creating web pages and web applications.
+         </definition>
+         <definition name="css" author="kartel">
+             A style sheet language used for describing the presentation of a document written in a markup language.
+         </definition>
+         <definition name="javascript" author="gully bop">
+             A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+         </definition>
+         <definition name="php" author="alkaline">
+             A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+         </definition>
+     
+ </entries>';
+  header('Content-Type: text/xml');
+ $xmlOutput = new SimpleXMLElement($xmldata);
+ if($all=='true'){
+     print $xmlOutput->asXML();
+ }else{
+     foreach($xmlOutput->definition as $def){
+         if((string)$def['name'] == $query){
+             $x = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><entries>'.$def->asXML().'</entries>');
+             print "<h3>".$query."</h3>";
+             print $x->asXML();
+             break;
+         }
+     }
+ }  
